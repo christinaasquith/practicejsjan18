c55a411741f923d6f5b2127cafc3770ef4a51a32
Christina Asquith <christinaasquith@gmail.com>
2021-01-19 10:14:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const color = document.querySelector ('#color')
+color.addEventListener('click', function() {
+    console.log ('change the color')
+    color.style.backgroundColor = 'pink'
+
+})
+
+const circle = document.querySelector('.circle')
+color.addEventListener('click', function() {
+    console.log ('change the image')
+    circle.style.backgroundCircle = 'image'
+
+})
+ 
